8a0cb36bb31a37b4814662618f5c8471e958f1ac
mastr777 <mstrandberg777@gmail.com>
2026-03-10 21:09:45 +0100

    remove document.write from js file

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ setInterval(() => {
   while (Date.now() - start < 1000) {}
 }, 3000)
 
-// Skriver ut HTML direkt i dokumentet
-document.write("<h1 style='color:black;'>HELLO FROM DOCUMENT.WRITE()</h1>")
-document.write("<p>Another document.write() call</p>")
-
 // Förhindrar användaren att navigera bakåt
 history.pushState(null, "", location.href)
 window.onpopstate = () => history.go(1)
